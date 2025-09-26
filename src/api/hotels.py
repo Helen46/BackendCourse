e773f094ac09c2b15e600657d8836e6a9a763250
Elena Prilutskay <a.priluckay87@gmail.com>
2025-09-26 14:45:41 +0300

    replace id query param with location query param

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -12,14 +12,14 @@ router = APIRouter(prefix="/hotels", tags=["Отели"])
 @router.get("", summary="Получение списка отелей")
 async def get_hotels(
         pagination: PaginationDep,
-        id: int | None = Query(None, description="Айдишник"),
+        location: str | None = Query(None, description="Адрес отеля"),
         title: str | None = Query(None, description="Название отеля"),
 ):
     per_page = pagination.per_page or 5
     async with async_session_maker() as session:
         query = select(HotelsOrm)
-        if id:
-            query = query.filter_by(id=id)
+        if location:
+            query = query.filter(HotelsOrm.location.like(f"%{location}%"))
         if title:
             query = query.filter_by(title=title)
         query = (
