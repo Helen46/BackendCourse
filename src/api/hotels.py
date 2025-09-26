6b182ae1cd94711e555407cd7ff6927e1cb7dee1
Elena Prilutskay <a.priluckay87@gmail.com>
2025-09-26 14:25:42 +0300

    add in get hotels filter_by, limit, offset

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -15,15 +15,22 @@ async def get_hotels(
         id: int | None = Query(None, description="Айдишник"),
         title: str | None = Query(None, description="Название отеля"),
 ):
+    per_page = pagination.per_page or 5
     async with async_session_maker() as session:
         query = select(HotelsOrm)
+        if id:
+            query = query.filter_by(id=id)
+        if title:
+            query = query.filter_by(title=title)
+        query = (
+            query
+            .limit(per_page)
+            .offset(per_page * (pagination.page - 1))
+        )
         result = await session.execute(query)
         hotels_ = result.scalars().all()
         # print(type(hotels_), hotels_)
         return hotels_
-
-        # if pagination.page and pagination.per_page:
-        #     return hotels_[pagination.per_page * (pagination.page - 1):][:pagination.per_page]
 
 
 @router.post("", summary="Добавление отеля",)
