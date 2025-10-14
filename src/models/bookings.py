256bfb32a946841564965408a0247371643ce1a3
Elena Prilutskay <a.priluckay87@gmail.com>
2025-10-14 23:30:15 +0300

    create router.post for bookings

--- src/models/bookings.py
+++ src/models/bookings.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from src.database import Base
 
 
-class BookingsORM(Base):
+class BookingsOrm(Base):
     __tablename__ = "bookings"
 
     id: Mapped[int] = mapped_column(primary_key=True)
