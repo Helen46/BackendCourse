0fab1fc2ab6948557a3aeeab773dc71fbaa00228
Elena Prilutskay <a.priluckay87@gmail.com>
2025-10-20 22:12:18 +0300

    create get_filtered_by_time() for HotelsRepository

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from fastapi import Query, APIRouter, Body
 
 from src.database import async_session_maker, engine
@@ -15,13 +17,20 @@ async def get_hotels(
         db: DBDep,
         location: str | None = Query(None, description="Локация"),
         title: str | None = Query(None, description="Название отеля"),
+        date_from: date = Query(example="2025-10-01"),
+        date_to: date = Query(example="2025-10-20"),
+
 ):
     per_page = pagination.per_page or 5
-    return await db.hotels.get_all(
-        location=location,
-        title=title,
-        limit=per_page,
-        offset=per_page * (pagination.page - 1)
+    # return await db.hotels.get_all(
+    #     location=location,
+    #     title=title,
+    #     limit=per_page,
+    #     offset=per_page * (pagination.page - 1)
+    # )
+    return await db.hotels.get_filtered_by_time(
+        date_from=date_from,
+        date_to=date_to
     )
 
 
